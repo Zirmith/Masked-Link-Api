4013b374515f1c43b8ce597ab2ccd39197f24ff0
ZDLua <hj_jr@icloud.com>
2023-03-08 08:18:24 -0600

    initial commit

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
 const validUrl = require('valid-url');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = 'mongodb+srv://Paragon:thevoidis2@sessions.m6cqqsq.mongodb.net/';
-const DB_NAME = 'masked-links';
 
-const client = new MongoClient(MONGO_URI);
+const links = {};
 
 app.use(express.json());
 app.use(cors());
-
-
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Masked Link API!');
@@ -20,7 +15,8 @@ app.get('/', (req, res) => {
 
 app.post('/links', async (req, res) => {
   const { url } = req.body;
- console.log(req.body);
+  console.log(req.body);
+
   if (!url) {
     return res.status(400).json({ error: 'Missing URL' });
   }
@@ -29,57 +25,30 @@ app.post('/links', async (req, res) => {
     return res.status(400).json({ error: 'Invalid URL' });
   }
 
-  try {
-    await client.connect();
-    const db = client.db(DB_NAME);
-    const collection = db.collection('links');
+  const link = Object.values(links).find(l => l.originalUrl === url);
 
-    let link = await collection.findOne({ originalUrl: url });
+  if (link) {
+    return res.status(200).json({ code: link.code, shortUrl: `https://masked-api.onrender.com/${link.code}` });
+  } else {
+    const code = Math.floor(Math.random() * 1000000);
+    const newLink = { code, originalUrl: url };
+    links[code] = newLink;
 
-    if (!link) {
-      const code = Math.floor(Math.random() * 1000000);
-
-      link = { code, originalUrl: url };
-
-      const result = await collection.insertOne(link);
-
-      if (result.insertedCount !== 1) {
-        throw new Error('Failed to create link');
-      }
-    }
-
-    return res.status(200).json({ code: link.code, shortUrl: `http://localhost:${PORT}/${link.code}` });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: 'Internal server error' });
-  } finally {
-    await client.close();
+    return res.status(200).json({ code: newLink.code, shortUrl: `https://masked-api.onrender.com/${newLink.code}` });
   }
 });
 
 app.get('/:code', async (req, res) => {
   const { code } = req.params;
 
-  try {
-    await client.connect();
-    const db = client.db(DB_NAME);
-    const collection = db.collection('links');
+  const link = links[code];
 
-    const link = await collection.findOne({ code: parseInt(code) });
-
-    if (link) {
-      return res.redirect(link.originalUrl);
-    }
-
-    return res.status(404).json({ error: 'Link not found' });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: 'Internal server error' });
-  } finally {
-    await client.close();
+  if (link) {
+    return res.redirect(link.originalUrl);
   }
-});
 
+  return res.status(404).json({ error: 'Link not found' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
